8e9bf32efc78ed3458f553541a55278c8ebb8481
Ahad <atmahad726@gmail.com>
2023-01-16 22:42:12 +0600

    New console project started and a package installed

---
+++ StudentHousing/StudentHousing/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace StudentHousing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            const string connectionString = "Server=DESKTOP-6U3O5E7; Database=MyMess; User Id=talha; Password=1998;";
+        }
+    }
+}
